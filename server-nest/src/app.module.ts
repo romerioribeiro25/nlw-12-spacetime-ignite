2bf151394ae77f1c6efd75fc93b1660f22d8be6e
Romerio Ribeiro <romerio111@hotmail.com>
2023-05-26 23:54:09 -0300

    feat(prima): module

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UploadModule } from './upload/upload.module'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [UploadModule],
+  imports: [UploadModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
