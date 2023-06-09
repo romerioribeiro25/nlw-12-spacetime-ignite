da8c207f2796dc7e665c9340520e261ef85aff2e
Romerio Ribeiro <romerio111@hotmail.com>
2023-06-09 19:08:52 -0300

    feat: add memories module

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -5,9 +5,10 @@ import { UploadModule } from './upload/upload.module'
 import { PrismaModule } from './prisma/prisma.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
+import { MemoriesModule } from './memories/memories.module';
 
 @Module({
-  imports: [UploadModule, PrismaModule, AuthModule, UsersModule],
+  imports: [UploadModule, PrismaModule, AuthModule, UsersModule, MemoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
